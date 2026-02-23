06808d370c6719e00a071693f49a97b1c3ddc35d
ShobhanaMangale <shobhanadmangale5@gmail.com>
2026-02-23 22:57:31 +0530

    Create TaskUpdate.java

---
+++ TaskUpdate.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class TaskUpdate {
+
+    private int id;
+    private String status;
+
+    public TaskUpdate(int id, String status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
